444fbd22ea71fce77fa3aa68aa49d8a0977ab159
Nicole Villalobos <nicolevillalobosbe@gmail.com>
2019-03-06 06:17:22 -0500

    agrege archivos

--- src/cipher.js
+++ src/cipher.js
@@ -54,9 +54,10 @@ window.cipher = {
     */
    //1paso recorrer
    for(let i=0;texto.length>i;i++){
-     
+
      //validamos si el codigo unicode es un caracdter o numero
-     if(texto.charCodeAt(i)>=32 &&texto.charCodeAt(i)<=64){
+    
+     if(texto.charCodeAt(i)>=32 && texto.charCodeAt(i)<=64){
     /** aqui se ejcuta codigo */
       let ascciUnicode=texto.charCodeAt(i);
       let ascciChart=String.fromCharCode(ascciUnicode);
@@ -64,26 +65,30 @@ window.cipher = {
      }
     
        /* 1.- minuscula , 2 mayuscula */
-       else if(texto.charCodeAt(i)>=65 &&texto.charCodeAt(i)<=95){
+       else if(texto.charCodeAt(i)>=65 &&texto.charCodeAt(i)<=90){
          //letras en mayuscula 65  a
          let ascciUnicode=(texto.charCodeAt(i)-65-parseInt(offset))%26 + 65;
          let ascciChart=String.fromCharCode(ascciUnicode);
          resultado+=ascciChart;
  
        }
-      
+    
+       
        else if(texto.charCodeAt(i)>=97 &&texto.charCodeAt(i)<=122){
            //letras en minuscula
            let ascciUnicode=(texto.charCodeAt(i)-97-parseInt(offset))%26 +97;
            let ascciChart=String.fromCharCode(ascciUnicode);
            resultado+=ascciChart;
          }
-   return resultado;
+   
         }
+        return resultado;
       }
     }
     
 
+
+    
 
 
 

8e04a488815e4c1ecbfef7f4b38a251a222d8a31
andrealemonroy <andreale17@icloud.com>
2019-03-11 08:24:57 -0500

    cambios por test

--- src/cipher.js
+++ src/cipher.js
@@ -8,7 +8,7 @@ window.cipher = {
      resultado+=String.fromCharCode((palabra-65+offset)%26+65);     
 }
 else if (palabra>=48 && palabra<=57) {
-  resultado+=String.fromCharCode((palabra-57+offset)%10+57);     
+  resultado+=String.fromCharCode((palabra-48+offset)%10+48);     
 }
 else if (palabra>=97 && palabra<=122){
 resultado+=String.fromCharCode((palabra-97+offset)%26+97);
@@ -23,22 +23,20 @@ else {
 
   decode: (texto,offset) => {
     let resultado= "";
+    let palabra="";
     for (let i =0;i<texto.length; i++) {
-      let palabra= texto.charCodeAt(i);
+     palabra= texto.charCodeAt(i);
    /*  codigo >=32 pero menor o igual 64 */
      if (palabra>=65 && palabra<=90) {
       resultado+=String.fromCharCode((palabra-90-offset)%26+90);     
+          } else if (palabra>=48 && palabra<=57) {
+            resultado+=String.fromCharCode((palabra-57-offset)%10+57);     
+          } else if (palabra>=97 && palabra<=122){
+           resultado+=String.fromCharCode((palabra-122-offset)%26+122);
+          }else {
+            resultado+=String.fromCharCode(palabra);
  }
- else if (palabra>=48 && palabra<=57) {
-  resultado+=String.fromCharCode((palabra-57-offset)%10+57);     
 }
-     else if (palabra>=97 && palabra<=122){
-      resultado+=String.fromCharCode((palabra-122-offset)%26+122);
- }
- else {
-   resultado+=String.fromCharCode(palabra);
- }
-  }
   return resultado;
-  }
+},
 }
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -13,6 +13,9 @@ describe('cipher', () => {
       it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33',()=>{
           assert.equal(cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ",33),"HIJKLMNOPQRSTUVWXYZABCDEFG");
       });
+      it('debería retornar 345678 para 012345 con offset 33',()=>{
+        assert.equal(cipher.encode('012345',33),'345678');
+     });
     });
   
     describe('cipher.decode', () => {
